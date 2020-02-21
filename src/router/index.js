6c15db07f3acfcd7ccd8ccb05feb85d4847db041
Jane <582194643@qq.com>
2020-02-21 11:44:59 +0800

    项目起步-整合路由，提供路由文件

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 1.导入vue
+import Vue from 'vue';
+// 2.导入路由
+import VueRouter from 'vue-router';
+// 3.注册路由
+Vue.use(VueRouter);
+// 4.准备组件
+// 5.创建路由对象，并配置路由规则
+const router= new VueRouter({
+    routes:[
+
+    ]
+});
+// 6.将路由对象暴露出去
+export default router;
