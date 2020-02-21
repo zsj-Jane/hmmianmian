c2d88175b5f2a7d16bc11e13789903b1c57f9f32
Jane <582194643@qq.com>
2020-02-21 11:54:03 +0800

    项目起步-整合element-ui

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import App from './App.vue'
 Vue.config.productionTip = false
 // 导入路由对象
 import router from './router/index';
+// 导入Element-ui
+import ElementUI from 'element-ui';
+// 导入Element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册Element-ui
+Vue.use(ElementUI);
+// 创建Vue实例，挂载到ip为app的div上
 new Vue({
   // 路由对象挂载到vue实例中
   router,
