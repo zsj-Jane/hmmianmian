e4d1aef042dcf7d52b056805f2ebfcfb9d8ce952
Jane <582194643@qq.com>
2020-02-21 11:48:48 +0800

    项目起步-整合路由，提供路由文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+// 导入vue
 import Vue from 'vue'
+// 导入根组件
 import App from './App.vue'
-
+// 是否提示生产信息
 Vue.config.productionTip = false
-
+// 导入路由对象
+import router from './router/index';
 new Vue({
+  // 路由对象挂载到vue实例中
+  router,
   render: h => h(App),
 }).$mount('#app')
