3cae45101d82328c5655409103b6cb527ead4aa5
Jane <582194643@qq.com>
2020-02-24 19:38:50 +0800

    首页模块-抽取用户信息获取 和 用户退出 方法

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+// 导入axios
+import axios from 'axios';
+// 导入获取token的文件
+import {getToken} from '@/utilts/token.js';
+// 克隆一个axios对象，并设置它的初始基因，
+// 以后通过indexRequest对象去发送axios请求，就会自动携带基地址和请求头
+let indexRequest = axios.create({
+    // 基地址
+    baseURL:process.env.VUE_APP_BASE_URL,
+    // 请求头
+    headers:{
+        token:getToken
+    }
+})
+// 获取当前登录的用户信息
+export function getInfo() {
+    return indexRequest({
+        url:"/info",
+        method:'get'
+    });
+}
+
+// 退出请求
+export function logout() {
+    return indexRequest({
+        url:"/logout",
+        method:'get'
+    });
+}
