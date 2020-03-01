6107fb7864bcd6495e961d892ee0ba4fec6aa375
Jane <582194643@qq.com>
2020-03-01 18:03:32 +0800

    首页模块-请求对象抽取

--- src/utilis/request.js
+++ src/utilis/request.js
@@ -1,20 +1,27 @@
 // 封装请求对象
 // 导入axios
 import axios from 'axios';
-// 导入操作token的文件
-import { getToken } from '@/utilis/token.js'
-// 创建一个新的请求对象
+// 导入获取token的文件
+import { getToken } from '@/utilis/token.js';
+// 克隆一个axios对象，并设置它的初始基因，
+// 以后通过indexRequest对象去发送axios请求，就会自动携带基地址和请求头
 let request = axios.create({
-    // 设置基地址
+    // 基地址
     baseURL: process.env.VUE_APP_BASE_URL,
-    // 允许携带cookie
-    withCredentials: true
+    // 请求头(由于index.js文件在项目已启动就执行，这里headers里的token值为null，使得登录进入首页后，无法获得个人信息)
+    // headers:{
+    //     token:getToken()
+    // }
 });
-// 添加请求拦截器
+// 添加请求拦截器（官网CV）
+// 只要是indexRequest发的请求都会被拦截下来
 request.interceptors.request.use(function (config) {
-    // 发送请求前做些什么
-    // 设置请求头的token
+    // config参数：被拦截下来的请求的配置信息
+    // window.console.log(config);
+    // 往config里面的headers添加一个token，就代表此次发送的请求中包含token
+    // 每次首页发送请求，都会被拦截下来，并自动加一个token
     config.headers.token = getToken();
+    // 在发送请求之前做些什么
     return config;
 }, function (error) {
     // 对请求错误做些什么
