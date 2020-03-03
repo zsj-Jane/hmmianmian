8755e9070a0bdc4e1de1892d70b3184922d8ae21
Jane <582194643@qq.com>
2020-03-03 11:02:23 +0800

    权限模块-按钮级别的权限管理

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -50,14 +50,14 @@
             <span v-else style="color:red;">禁用</span>
           </template>
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" v-if="['超级管理员', '管理员', '老师'].includes($store.state.role)">
           <template slot-scope="scope">
             <el-button type="text" @click="handleEdit(scope.row)">编辑</el-button>
             <el-button
               type="text"
               @click="changeStatus(scope.row)"
             >{{scope.row.status===1?'禁用':'启用'}}</el-button>
-            <el-button type="text" @click="handleDelete(scope.row)">删除</el-button>
+            <el-button type="text" @click="handleDelete(scope.row)" v-if="['超级管理员', '管理员'].includes($store.state.role)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
