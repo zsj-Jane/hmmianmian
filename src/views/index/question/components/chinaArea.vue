ad8b46aaba477d681fe7123f4a3a0c546f16c7b9
Jane <582194643@qq.com>
2020-03-04 20:30:05 +0800

    题库模块-省市区级联选择器 抽取

---
+++ src/views/index/question/components/chinaArea.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-cascader
+    v-model="city"
+    :options="options"
+    :props="{ expandTrigger: 'hover' ,value:'label'}"
+    @change="handleChange"
+  ></el-cascader>
+</template>
+
+<script>
+// 导入带省市区以及全部选项的省市区联动数据
+import { regionDataPlus } from "element-china-area-data";
+export default {
+  name: "chinaArea",
+  props: {
+    // 父传子
+    value: {
+      default: ""
+    }
+  },
+  data() {
+    return {
+      // 级联选择器的选项 联动数据：城市
+      options: regionDataPlus,
+      city: this.value
+    };
+  },
+  methods: {
+    handleChange() {
+      // 子传父
+      this.$emit("input", this.city);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
